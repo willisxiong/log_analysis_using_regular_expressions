b5b3f41ea4f7457535d91467f17b66fe09ce0d83
willisxiong <xiongwei.fly@gmail.com>
2022-03-26 12:10:10 +0800

    modify the error_report and user_report, add class container to simplify the code

--- error_report.py
+++ error_report.py
@@ -1,61 +1,77 @@
 #!/usr/bin/env python3
 
 import re
-import sys
+import os
 import csv
 import subprocess
 import pandas as pd
+ 
+class ErrorReport:
+    def __init__(self,filename):
+        self.filename = filename
 
-filename = sys.argv[1]
+    def get_error_msg(self):
+        """find the error message in syslog.log and return a error message list"""
+        pattern = r"ERROR([a-zA-Z' ]+)"
+        error_msg = []
+        with open(self.filename) as f:
+            for line in f:
+                if "ERROR" in line:
+                    error_line = re.search(pattern, line)
+                    new_error_line = error_line[1].strip()
+                    error_msg.append(new_error_line)
+                continue
+        return error_msg
 
-#find all the error message
-def get_error_msg(filename):
-    pattern = r"ERROR([a-zA-Z' ]+)"
-    error_msg = []
-    with open(filename) as f:
-        for line in f:
-            if "ERROR" in line:
-                error_line = re.search(pattern, line)
-                new_error_line = error_line[1].strip()
-                error_msg.append(new_error_line)
-            continue
-    return error_msg        
-all_error_msg = get_error_msg(filename)
+    def count_msg(self):
+        """count for each error and return a dict"""
+        msg_list = []
+        msg_dict = {}
+        lmsg = self.get_error_msg()
+        for msg in lmsg:
+            if not msg in msg_list:
+                msg_list.append(msg)
+                msg_dict[msg] = 1
+            else:
+                msg_dict[msg] += 1
+        return msg_dict    
 
-#count for each error
-def count_msg(lmsg):
-    msg_list = []
-    msg_dict = {}
-    for msg in lmsg:
-        if not msg in msg_list:
-            msg_list.append(msg)
-            msg_dict[msg] = 1
+    def convt_data(self):
+        """change the msg dict to a desired format for write into csv"""
+        msg_data = {}
+        csv_data = []
+        orig_data = self.count_msg()
+        for k, v in orig_data.items():
+            msg_data["ERROR"] = k
+            msg_data["COUNT"] = v
+            csv_data.append(msg_data.copy())
+        return csv_data
+
+    def create_report(self):
+        """
+        order the csv data by descending and create the csv file,
+        print out the csv content
+        """
+        error_report_data = self.convt_data()
+        sorted_data = sorted(error_report_data, key=lambda error_report: error_report["COUNT"], reverse=True)
+
+        keys = ["ERROR", "COUNT"]
+
+        with open('error_report.csv', 'w') as report:
+            writer = csv.DictWriter(report, fieldnames = keys)
+            writer.writeheader()
+            writer.writerows(sorted_data)
+
+        subprocess.run(['cat', 'error_report.csv'])
+
+    def create_html(self):
+        if os.path.exists("error_report.csv"):
+            df = pd.read_csv("error_report.csv")
+            df.to_html("error_report.html")
         else:
-            msg_dict[msg] += 1
-    return msg_dict
-error_msg_result = count_msg(all_error_msg)
+            print("Please generate the csv file first")
 
-#convert the error dict to a lisf of dict format
-#in order to create data for write into csv
-def convt_data(orig_data):
-    msg_data = {}
-    csv_data = []
-    for k, v in orig_data.items():
-        msg_data["ERROR"] = k
-        msg_data["COUNT"] = v
-        csv_data.append(msg_data.copy())
-    return csv_data 
-error_report_data = convt_data(error_msg_result)
-sorted_report = sorted(error_report_data, key=lambda error_report: error_report["COUNT"], reverse=True)
 
-keys = ["ERROR", "COUNT"]
-
-with open('error_report.csv', 'w') as report:
-    writer = csv.DictWriter(report, fieldnames = keys)
-    writer.writeheader()
-    writer.writerows(sorted_report)
-
-subprocess.run(['cat', 'error_report.csv'])
-
-df = pd.read_csv("error_report.csv")
-html = df.to_html("error_report.html")
+report = ErrorReport("syslog.log")
+report.create_report()
+report.create_html()
--- user_report.py
+++ user_report.py
@@ -1,53 +1,67 @@
 #!/usr/bin/env python3
 
 import re
-import sys
+import os
 import csv
 import subprocess
 import pandas as pd
 
-filename = sys.argv[1]
+class UserReport:
+    def __init__(self, filename):
+        self.filename = filename
 
-#get all the username and store in a list
-def get_user_msg(filename):
-    pattern = r"\(([a-z.]+)\)"
-    user_list = []
-    with open(filename) as file:     
-        for line in file:
-            usr_line = re.search(pattern, line)
-            if not usr_line[1] in user_list:
-                user_list.append(usr_line[1])
-    return user_list
-user_list = get_user_msg(filename)
-
-#count INFO and ERROR for each user
-#put all the count data into list dict format
-def count_msg(userlist):
-    user_dict = []
-    each_user = {}
-    for user in userlist:
-        each_user["USERNAME"] = user
-        each_user["INFO"] = 0
-        each_user["ERROR"] = 0
-        with open(filename) as file:
+    def get_user_msg(self):
+        """find all the username in syslog.log and store them in a list"""
+        pattern = r"\(([a-z.]+)\)"
+        user_list = []
+        with open(self.filename) as file:     
             for line in file:
-                if user in line and "INFO" in line:
-                    each_user["INFO"] += 1
-                elif user in line and "ERROR" in line:
-                    each_user["ERROR"] += 1
-                continue
-            user_dict.append(each_user.copy())
-    return user_dict
-csv_data = count_msg(user_list)
+                usr_line = re.search(pattern, line)
+                if not usr_line[1] in user_list:
+                    user_list.append(usr_line[1])
+        return user_list
 
-keys = ["USERNAME", "INFO", "ERROR"]
+    def count_msg(self):
+        """
+        count INFO and ERROR for each user in syslog.log,
+        put all the count data into a list dict format
+        """
+        user_dict = []
+        each_user = {}
+        userlist = self.get_user_msg()
+        for user in userlist:
+            each_user["USERNAME"] = user
+            each_user["INFO"] = 0
+            each_user["ERROR"] = 0
+            with open(self.filename) as file:
+                for line in file:
+                    if user in line and "INFO" in line:
+                        each_user["INFO"] += 1
+                    elif user in line and "ERROR" in line:
+                        each_user["ERROR"] += 1
+                    continue
+                user_dict.append(each_user.copy())
+        return user_dict
+    
+    def create_report(self):
+        """create the csv file and print it out"""
+        keys = ["USERNAME", "INFO", "ERROR"]
+        csv_data = self.count_msg()
 
-with open("user_report.csv", "w") as user:
-    writer = csv.DictWriter(user, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(csv_data)
+        with open("user_report.csv", "w") as user:
+            writer = csv.DictWriter(user, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(csv_data)
 
-subprocess.run(['cat', 'user_report.csv'])
+        subprocess.run(['cat', 'user_report.csv'])
 
-df = pd.read_csv("user_report.csv")
-html = df.to_html("user_report.html")
+    def create_html(self):
+        if os.path.exists("user_report.csv"):
+            df = pd.read_csv("user_report.csv")
+            df.to_html("user_report.html")
+        else:
+            print("Please generate the csv file first")
+
+report = UserReport("syslog.log")
+report.create_report()
+report.create_html()
